794f053c8635d00cceb3478b8b2faabf336cdd76
Rukorio-Vanessa <blwwbl55@gmail.com>
2023-03-05 22:42:43 +0300

    created projects columns

--- db/migrate/20230305192848_create_projects.rb
+++ db/migrate/20230305192848_create_projects.rb
@@ -1,4 +1,9 @@
 class CreateProjects < ActiveRecord::Migration[6.1]
   def change
+    t.integer :user_id, null: false
+    t.string :title, null: false
+    t.string :description
+    t.string :current_state, null: false
+    t.timestamps
   end
 end
