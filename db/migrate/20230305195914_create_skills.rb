0ffa07a351f0f518835a3a271141b3b705f90b26
Rukorio-Vanessa <blwwbl55@gmail.com>
2023-03-05 23:05:23 +0300

    created skills columns

--- db/migrate/20230305195914_create_skills.rb
+++ db/migrate/20230305195914_create_skills.rb
@@ -1,4 +1,8 @@
 class CreateSkills < ActiveRecord::Migration[6.1]
   def change
+    create_table :users do |t|
+      t.string :name, null: false
+      t.integer :user_id, null: false
+    end
   end
 end
