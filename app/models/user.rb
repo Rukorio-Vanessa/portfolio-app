d3ad54080e08a1f16e2f8d6bb9bd694c072d2874
Rukorio-Vanessa <blwwbl55@gmail.com>
2023-03-06 01:21:51 +0300

    defined function to store and secure password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,25 @@
+require 'bcrypt'
+
 class User < ActiveRecord::Base
     has_many :projects
     has_many :skills
+
+include BCrypt
+
+#acquired from the bcrypt github repository
+def password
+    @password ||= Password.new(password_hash)
+  end
+
+def password=(new_password)
+    @password = Password.create(new_password)
+    self.password_hash = @password
+end
+
+end
+
+def authenticate(password)
+    self.password == password
+end
     
 end
